75863ea3a47f937a9216389da6ee2fbbe3c6441f
anathsu <willian.silva@al.infnet.edu.br>
2021-05-04 12:12:19 -0300

    Tentativa de conexão do Heroku com MongoDB_URI

--- database/index.js
+++ database/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+mongodb+srv://useradmin:useradmin121212@nswt-db.w52aj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/apiNSWT', {
-    useMongoClient: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/apiNSWT', {
+    useMongoClient: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Conectado ao banco de dados MongoDB com sucesso!")
 }).catch((err) => {
