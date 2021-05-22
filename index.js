a1ca1dfaca4db5b3dfd5375dadda67448fcb2715
anathsu <willian.silva@al.infnet.edu.br>
2021-05-21 22:40:17 -0300

    Retorno do true no urlencoded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
